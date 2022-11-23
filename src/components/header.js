597356eb23149a0c3ebe09ffc15390913eaccf9e
Carlo Perito <carlop@Carlos-MBP.home>
2022-11-23 09:10:41 -0800

    centering banner text

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,52 @@
 import React from 'react'
 import '../styles/header.css'
 import cover from '../assets/cover.png'
+import { BsHouseDoor } from 'react-icons/bs'
+import { FiHeart } from 'react-icons/fi'
+import { AiOutlineMail } from 'react-icons/ai'
+
 
 
 function Header() {
 
+    const bannerStyle = {
+        backgroundImage: `url(${cover})`,
+        height: '60vh',
+        width: '100%',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        position: 'relative'
+    }
+
+    const bannerText = {
+        color: 'white',
+        fontSize: '24px',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+    }
+
+
     return (
+
         <div className="header-container">
             <div className="nav-container">
                 <div className="nav-left">
-                    UniLife
+                    <p><BsHouseDoor /></p>
+                    <p>UniLife</p>
                 </div>
                 <div className="nav-right">
+                    <p><FiHeart /></p>
                     <p>Shortlist</p>
+                    <p><AiOutlineMail /></p>
                     <p>Contact Us</p>
                 </div>
             </div>
-            <div>
-                <img clasName="banner" style={{ width: '100%' }} src={cover} />
+            <div style={bannerStyle}>
+                <p style={bannerText}>Find Student Homes with Bills Included</p>
             </div>
-        </div>
 
+        </div >
 
 
 
