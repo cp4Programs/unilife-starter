f091e5c43c5506abc2cf11c1bb7bffdd3519f67f
Carlo Perito <carlop@Carlos-MBP.home>
2023-01-09 00:20:24 -0800

    component css styles overlapping one another

--- src/components/HomeDetailsPrices.js
+++ src/components/HomeDetailsPrices.js
@@ -14,22 +14,27 @@ function HomeDetailsPrices({ propertyData }) {
                 <div className="bedroom-prices-table">
                     <div className="top-row-line">
                         <p>Bedroom 1</p>
-                        <p></p>
+                        <p>{propertyData.bedroom_prices.bedroom_one}</p>
                     </div>
                     <div className="row-lines">
                         <p>Bedroom 2</p>
+                        <p>{propertyData.bedroom_prices.bedroom_two}</p>
                     </div>
                     <div className="row-lines">
                         <p>Bedroom 3</p>
+                        <p>{propertyData.bedroom_prices.bedroom_three}</p>
                     </div>
                     <div className="row-lines">
                         <p>Bedroom 4</p>
+                        <p>{propertyData.bedroom_prices.bedroom_four}</p>
                     </div>
                     <div className="row-lines">
                         <p>Bedroom 5</p>
+                        <p>{propertyData.bedroom_prices.bedroom_five}</p>
                     </div>
                     <div className="row-lines">
                         <p>Bedroom 6</p>
+                        <p>{propertyData.bedroom_prices.bedroom_six}</p>
                     </div>
                 </div>
             </div>
